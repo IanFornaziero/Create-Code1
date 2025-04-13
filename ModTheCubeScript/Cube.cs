938285b89ed091d1e249154e7943cd2bda5ebfc4
IanFornaziero <capitaoian115@gmail.com>
2025-04-13 19:56:24 -0300

    Add files via upload

---
+++ ModTheCubeScript/Cube.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    private float velocidade = 2.0f;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 1.3f;
+
+        InvokeRepeating("MudarCor", 1.0f, 2.0f);
+
+    }
+    
+    void Update()
+    {
+        transform.Rotate(200.0f * Time.deltaTime, 200.0f * Time.deltaTime, 200.0f * Time.deltaTime);
+        transform.localScale += Vector3.one * velocidade * Time.deltaTime;
+
+        if(transform.localScale.x > 5.0f)
+        {
+            transform.localScale = Vector3.one;
+        }
+    }
+
+    void MudarCor()
+    {
+        Material material = Renderer.material;
+        material.color = new Color(Random.Range(0, 2), Random.Range(0, 2), Random.Range(0, 2), Random.Range(0, 2));
+    }
+}
